1b4a0467bf30a0b83a7b543012ce2909e9cc0aae
茅野誠也 <anu@chinoseiyanoMacBook-Air.local>
2022-12-23 15:33:35 +0900

    Add bounding box to result_img

--- module_face.py
+++ module_face.py
@@ -21,6 +21,7 @@ class face_recognition():
         
     def profile(self):
         flag=0
+        #create dataframe
         emo_column = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
         df_emotion = pd.DataFrame(columns = emo_column)
         age_series = pd.Series(name='age')
@@ -31,12 +32,24 @@ class face_recognition():
             #replace BGR to RGB
             img3 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
             result = DeepFace.analyze(img3,actions=['emotion','age','gender'])
-            box = DeepFace.detectFace(img3, align=False)
-            box = np.clip(box * 255, 0, 255).astype(np.uint8)
-            tmp = cv2.imwrite('result/result_img/'+ str(flag) + '.png', box)
+            #create bounding box
+            #decide region
+            region = result['region']
+            x, y, w, h = region['x'], region['y'], region['w'], region['h']
+            #create mask
+            mask = np.full((img3.shape[0], img3.shape[1]),False)
+            #create line of bounding box
+            mask[y:y+3, x: x+w] = True  # upper width
+            mask[y+h:y+h+3, x: x+w] = True  # lower width
+            mask[y: y+h, x:x+3] = True  # left height
+            mask[y: y+h, x+w-3:x+w] = True  # right height
+            #merge mask to img
+            img3[mask==True] = 128
+            #replace BGR to RGB
+            img4 = cv2.cvtColor(img3, cv2.COLOR_BGR2RGB)
+            judgement = cv2.imwrite('result/result_img/' + str(flag) + '.png', img4)            
             flag += 1
-            print(tmp)
-            
+            print(judgement)
             emotion = result['emotion']
             emotion_list = []
             #create dataframe of emotion
@@ -52,4 +65,5 @@ class face_recognition():
         return output
     
     def export_csv(self):
+        #export csv from profiled data
         self.output.to_csv('./result/result.csv')
